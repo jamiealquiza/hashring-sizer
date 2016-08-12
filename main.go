f411107cfb2d3bfd917808e5a234e9c488839339
Jamie Alquiza <jamiealquiza@gmail.com>
2016-08-12 21:04:01 +0000

    it's friday, don't even bother

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"sort"
+	"time"
 
 	ch "github.com/jamiealquiza/polymur/consistenthash"
 )
-
 
 func main() {
 	f, err := os.Open("keys.txt")
@@ -23,7 +25,7 @@ func main() {
 		keys = append(keys, scanner.Text())
 	}
 
-	ring := ch.HashRing{Vnodes: 100}
+	ring := ch.HashRing{Vnodes: 100000}
 
 	for i := 0; i < 10; i++ {
 		node := fmt.Sprintf("this-is-node-%d", i)
@@ -31,13 +33,60 @@ func main() {
 	}
 
 	nodeCount := make(map[string]int)
+	lat := make([]time.Duration, len(keys))
 
-	for _, k := range keys {
+	for i, k := range keys {
+		s := time.Now()
 		n, _ := ring.GetNode(k)
-		nodeCount[n]++	
+		lat[i] = time.Since(s)
+		nodeCount[n]++
 	}
 
+	// Print node counts.
 	for k, v := range nodeCount {
 		fmt.Printf("%s: %d\n", k, v)
+	}
+
+	// Get min/max latency.
+	var highest time.Duration
+	lowest := 10 * time.Second
+	for _, v := range lat {
+		if v > highest {
+			highest = v
+		}
+		if v < lowest {
+			lowest = v
+		}
+	}
+
+	fmt.Printf("Max latency: %s\n", highest)
+	fmt.Printf("Min latency: %s\n", lowest)
+
+	// Get sorted latencies.
+	latInt := make([]int, len(lat))
+	for n := range lat {
+		latInt[n] = int(lat[n])
+	}
+
+	sort.Ints(latInt)
+
+	for n := range lat {
+		lat[n] = time.Duration(latInt[n])
+	}
+
+	// Get other stuff.
+	var latTotal time.Duration
+	for _, v := range lat {
+		latTotal += v
+	}
+	fmt.Printf("Avg. lantency: %s\n",
+		time.Duration(int(latTotal)/len(lat)))
+
+	if len(lat)%2 == 0 {
+		med := (lat[len(lat)/2] + lat[(len(lat)/2)+1]) / 2
+		fmt.Printf("Median latency: %s\n", med)
+	} else {
+		fmt.Printf("Median latency: %s\n",
+			lat[int(math.Floor(float64(len(lat)/2)))])
 	}
 }
